1acff4a2f671381b84d06d0055d66d9a68cfcbf8
Tobias Lounsbury <TobiasLounsbury@gmail.com>
2022-09-06 09:22:21 -0700

    Updated the call to formatMoney so it is a local assignment with null coalescence fallback for the change in function scope over the civicrm versions.

--- pricesetlogic_47.js
+++ pricesetlogic_47.js
@@ -9,9 +9,10 @@ CRM.$(function ($) {
   /***************[  ]***************/
   CRM.PricingLogic.ChangePrice = function (value) {
     //console.log(value);
-    var obj;
-    var newPrice;
-    var priceText;
+    let obj;
+    let newPrice;
+    let priceText;
+    let format$ = CRM.formatMoney || formatMoney;
 
     switch (CRM.PricingLogic.AllPriceFields[value.field].html_type) {
 
@@ -24,20 +25,20 @@ CRM.$(function ($) {
           break;
         }
 
-        var optObj = obj.find("option[value='" + value.option + "']");
+        let optObj = obj.find("option[value='" + value.option + "']");
         //Fetch the price data
-        eval('var option = ' + obj.attr('price'));
+        eval('let option = ' + obj.attr('price'));
         newPrice = parseFloat(value.price);
 
         //Fetch the list of additional price data
-        var selectValues = obj.data("price-field-values");
+        let selectValues = obj.data("price-field-values");
         option[value.option] = newPrice + optionSep + optionSep;
         selectValues[value.option].amount = newPrice;
 
         //Display the Price for the Option
-        var field_name = optObj.text();
+        let field_name = optObj.text();
         field_name = field_name.substring(0, field_name.indexOf('-')).trim();
-        priceText = formatMoney(newPrice, 2, separator, thousandMarker);
+        priceText = format$(newPrice, 2, separator, thousandMarker);
         optObj.text(field_name + " - " + symbol + " " + priceText);
 
         //Save the update price data
@@ -63,7 +64,7 @@ CRM.$(function ($) {
           break;
         }
 
-        eval('var option = ' + obj.attr('price'));
+        eval('let option = ' + obj.attr('price'));
         ele = option[0];
 
         newPrice = parseFloat(value.price);
@@ -71,11 +72,11 @@ CRM.$(function ($) {
         //set the data-amount
         obj.attr('data-amount', newPrice);
 
-        priceText = symbol + " " + formatMoney(newPrice, 2, window.separator, window.thousandMarker);
+        priceText = symbol + " " + format$(newPrice, 2, window.separator, window.thousandMarker);
 
         if (obj.attr("type") == "radio") {
 
-          var dataAmountValues = obj.data("price-field-values");
+          let dataAmountValues = obj.data("price-field-values");
           dataAmountValues[value.option].amount = newPrice;
 
           //change the Label
@@ -83,7 +84,7 @@ CRM.$(function ($) {
           calculateRadioLineItemValue(obj[0]);
         } else if (obj.attr("type") == "checkbox") {
           //change the Label
-          var field_label = obj.next().find(".crm-price-amount-label").text();
+          let field_label = obj.next().find(".crm-price-amount-label").text();
           obj.next().html("<span class='crm-price-amount-label'>" + field_label  + "</span> - " + priceText);
           calculateCheckboxLineItemValue(obj[0]);
         }
@@ -101,20 +102,20 @@ CRM.$(function ($) {
         }
 
         //Parse the price object
-        eval('var option = ' + obj.attr('price'));
-        var ele = option[0];
+        eval('let option = ' + obj.attr('price'));
+        let ele = option[0];
         //set the new price.
         newPrice = parseFloat(value.price);
         obj.attr('price', '["' + ele + '", "' + newPrice.toFixed(2) + optionSep + optionSep + '"]');
 
         //Calculate the line total
-        var qty = parseInt(obj.val()) || 0;
-        var rawTotal = qty * newPrice;
+        let qty = parseInt(obj.val()) || 0;
+        let rawTotal = qty * newPrice;
 
         //set the line total
         obj.attr("line_raw_total", rawTotal);
 
-        priceText = symbol + " " + formatMoney(newPrice, 2, window.separator, window.thousandMarker);
+        priceText = symbol + " " + format$(newPrice, 2, window.separator, window.thousandMarker);
         $("#price_" + value.field).parent().find("span.price-field-amount").text(priceText);
         obj.keyup();
         break;
@@ -130,7 +131,7 @@ CRM.$(function ($) {
    */
   CRM.PricingLogic.ChangePrices = function (values) {
     if ($.isArray(values)) {
-      for(var i in values) {
+      for(let i in values) {
         CRM.PricingLogic.ChangePrice(values[i]);
       }
     }
@@ -144,8 +145,8 @@ CRM.$(function ($) {
    * @param fields
    */
   CRM.PricingLogic.setToDefaultPricing = function(fields) {
-    for(var i in fields) {
-      var opts = {};
+    for(let i in fields) {
+      let opts = {};
       opts.field = fields[i].field;
       if(fields[i].hasOwnProperty("option")) {
         opts.option = fields[i].option;
